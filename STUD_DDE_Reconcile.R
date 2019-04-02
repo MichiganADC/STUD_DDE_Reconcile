a78b65fb0df610cafc4af427f6a3b86a31a24638
L. D. Nicolas May <LDNicolasMay@gmail.com>
2019-04-02 15:11:46 -0400

    working script; iterates over many files

--- STUD_DDE_Reconcile.R
+++ STUD_DDE_Reconcile.R
@@ -1,299 +1,48 @@
+#!/usr/bin/env Rscript
+
 # STUD_DDE_Reconcile.R
 
 # Load Libraries ----
 
-library(dplyr)
-library(readxl)
+suppressMessages( library(dplyr)   )
+suppressMessages( library(readxl)  )
+suppressMessages( library(stringr) )
+suppressMessages( library(crayon)  )
+
+source("STUD_DDE_Reconcile_helpers.R")
 
 
-# Get Data ----
+# Print initial message
+cat("\nComparing files...\n\n")
 
-# _ Match
+# Get list of files in source director "Summaries"
+path_to_summaries <- "Summaries/"
+files_list <- list.files(path_to_summaries)
+uniq_ids <- files_list %>% 
+  str_match("^BH19STUD\\d{5}") %>% 
+  unique() %>% 
+  as.vector()
 
-df_cor_evalpre <- 
-  read_xlsx("Correct_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Evaluation (Pre)",
-            range = "A1:L39",
-            col_names = LETTERS[1:which(LETTERS == "L")],
-            col_types = "text")
-df_cor_evalpost <-
-  read_xlsx("Correct_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Evaluation (Post)",
-            range = "A1:L39",
-            col_names = LETTERS[1:which(LETTERS == "L")],
-            col_types = "text")
-df_cor_tbpre <-
-  read_xlsx("Correct_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Toolbox (Pre)",
-            range = "A1:K7",
-            col_names = LETTERS[1:which(LETTERS == "K")],
-            col_types = "text")
-df_cor_tbpost <-
-  read_xlsx("Correct_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Toolbox (Post)",
-            range = "A1:K7",
-            col_names = LETTERS[1:which(LETTERS == "K")],
-            col_types = "text")
-df_cor_sess1 <-
-  read_xlsx("Correct_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 1",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_cor_sess2 <- 
-  read_xlsx("Correct_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 2",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_cor_sess3 <-
-  read_xlsx("Correct_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 3",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_cor_sess4 <-
-  read_xlsx("Correct_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 4",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-dfs_cor1 <- list(df_cor_evalpre  = df_cor_evalpre,
-                df_cor_evalpost = df_cor_evalpost,
-                df_cor_tbpre    = df_cor_tbpre,
-                df_cor_tbpost   = df_cor_tbpost,
-                df_cor_sess1    = df_cor_sess1,
-                df_cor_sess2    = df_cor_sess2,
-                df_cor_sess3    = df_cor_sess3,
-                df_cor_sess4    = df_cor_sess4)
-
-df_cor_evalpre <- 
-  read_xlsx("Correct_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Evaluation (Pre)",
-            range = "A1:L39",
-            col_names = LETTERS[1:which(LETTERS == "L")],
-            col_types = "text")
-df_cor_evalpost <-
-  read_xlsx("Correct_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Evaluation (Post)",
-            range = "A1:L39",
-            col_names = LETTERS[1:which(LETTERS == "L")],
-            col_types = "text")
-df_cor_tbpre <-
-  read_xlsx("Correct_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Toolbox (Pre)",
-            range = "A1:K7",
-            col_names = LETTERS[1:which(LETTERS == "K")],
-            col_types = "text")
-df_cor_tbpost <-
-  read_xlsx("Correct_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Toolbox (Post)",
-            range = "A1:K7",
-            col_names = LETTERS[1:which(LETTERS == "K")],
-            col_types = "text")
-df_cor_sess1 <-
-  read_xlsx("Correct_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 1",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_cor_sess2 <- 
-  read_xlsx("Correct_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 2",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_cor_sess3 <-
-  read_xlsx("Correct_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 3",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_cor_sess4 <-
-  read_xlsx("Correct_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 4",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-dfs_cor2 <- list(df_cor_evalpre  = df_cor_evalpre,
-                df_cor_evalpost = df_cor_evalpost,
-                df_cor_tbpre    = df_cor_tbpre,
-                df_cor_tbpost   = df_cor_tbpost,
-                df_cor_sess1    = df_cor_sess1,
-                df_cor_sess2    = df_cor_sess2,
-                df_cor_sess3    = df_cor_sess3,
-                df_cor_sess4    = df_cor_sess4)
-
-identical(dfs_cor1, dfs_cor2)
-
-# _ Mismatch
-
-df_inc_evalpre <- 
-  read_xlsx("Incorrect_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Evaluation (Pre)",
-            range = "A1:L39",
-            col_names = LETTERS[1:which(LETTERS == "L")],
-            col_types = "text")
-df_inc_evalpost <-
-  read_xlsx("Incorrect_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Evaluation (Post)",
-            range = "A1:L39",
-            col_names = LETTERS[1:which(LETTERS == "L")],
-            col_types = "text")
-df_inc_tbpre <-
-  read_xlsx("Incorrect_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Toolbox (Pre)",
-            range = "A1:K7",
-            col_names = LETTERS[1:which(LETTERS == "K")],
-            col_types = "text")
-df_inc_tbpost <-
-  read_xlsx("Incorrect_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Toolbox (Post)",
-            range = "A1:K7",
-            col_names = LETTERS[1:which(LETTERS == "K")],
-            col_types = "text")
-df_inc_sess1 <-
-  read_xlsx("Incorrect_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 1",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_inc_sess2 <- 
-  read_xlsx("Incorrect_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 2",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_inc_sess3 <-
-  read_xlsx("Incorrect_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 3",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_inc_sess4 <-
-  read_xlsx("Incorrect_1_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 4",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-dfs_inc1 <- list(df_inc_evalpre  = df_inc_evalpre,
-                df_inc_evalpost = df_inc_evalpost,
-                df_inc_tbpre    = df_inc_tbpre,
-                df_inc_tbpost   = df_inc_tbpost,
-                df_inc_sess1    = df_inc_sess1,
-                df_inc_sess2    = df_inc_sess2,
-                df_inc_sess3    = df_inc_sess3,
-                df_inc_sess4    = df_inc_sess4)
-
-df_inc_evalpre <- 
-  read_xlsx("Incorrect_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Evaluation (Pre)",
-            range = "A1:L39",
-            col_names = LETTERS[1:which(LETTERS == "L")],
-            col_types = "text")
-df_inc_evalpost <-
-  read_xlsx("Incorrect_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Evaluation (Post)",
-            range = "A1:L39",
-            col_names = LETTERS[1:which(LETTERS == "L")],
-            col_types = "text")
-df_inc_tbpre <-
-  read_xlsx("Incorrect_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Toolbox (Pre)",
-            range = "A1:K7",
-            col_names = LETTERS[1:which(LETTERS == "K")],
-            col_types = "text")
-df_inc_tbpost <-
-  read_xlsx("Incorrect_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Toolbox (Post)",
-            range = "A1:K7",
-            col_names = LETTERS[1:which(LETTERS == "K")],
-            col_types = "text")
-df_inc_sess1 <-
-  read_xlsx("Incorrect_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 1",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_inc_sess2 <- 
-  read_xlsx("Incorrect_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 2",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_inc_sess3 <-
-  read_xlsx("Incorrect_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 3",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-df_inc_sess4 <-
-  read_xlsx("Incorrect_2_Neuropsych_Summary_with_Daily_Task_Printouts.xlsx",
-            sheet = "Session 4",
-            range = "A1:E41",
-            col_names = LETTERS[1:which(LETTERS == "E")],
-            col_types = "text")
-dfs_inc2 <- list(df_inc_evalpre  = df_inc_evalpre,
-                df_inc_evalpost = df_inc_evalpost,
-                df_inc_tbpre    = df_inc_tbpre,
-                df_inc_tbpost   = df_inc_tbpost,
-                df_inc_sess1    = df_inc_sess1,
-                df_inc_sess2    = df_inc_sess2,
-                df_inc_sess3    = df_inc_sess3,
-                df_inc_sess4    = df_inc_sess4)
-
-identical(dfs_inc1, dfs_inc2)
-
-names(dfs_inc1)
-for (df_name in names(dfs_inc1)) {
-  compare_dfs(dfs_inc1[[df_name]], dfs_inc2[[df_name]])
-}
-
-compare_dfs(dfs_inc1[["df_inc_evalpre"]], dfs_inc2[["df_inc_evalpre"]])
-
-rlang::sym("blah")
-rlang::as_string(rlang::sym("blah"))
-
-
-compare_dfs <- function(df_1, df_2) {
-  df_1_name <- rlang::as_string(rlang::as_string(rlang::ensym(df_1)))
-  df_2_name <- rlang::as_string(rlang::as_string(rlang::ensym(df_2)))
+# Loop over each unique ID
+for (id in uniq_ids) {
   
-  if (identical(df_1, df_2)) {
-    cat(paste0(df_1_name, " / ", df_2_name, " identical.\n"))
-  } else {
-    cat(paste0(df_1_name, " / ", df_2_name, " NOT identical.\n"))
-    for (i in seq_len(nrow(df_1))) {
-      for (j in seq_len(ncol(df_1))) {
-        if (!identical(df_1[[i, j]], df_2[[i, j]])) {
-          cat(paste0("  Mismatch at ", df_1_name, " / ", df_2_name, ": ",
-                     "Row ", i, ", Column ", LETTERS[j], "\n"))
-        }
-      } # end for j
-    } # end for i
-  } # end if-else
-} 
-
-compare_dfs_list <- function(dfs_list_1, dfs_list_2) {
-  dfs_list_1_name <- rlang::as_string(rlang::ensym(dfs_list_1))
-  dfs_list_2_name <- rlang::as_string(rlang::ensym(dfs_list_2))
-  print(dfs_list_1_name)
-  print(dfs_list_2_name)
+  cat(bold(cyan(paste0("STUD ID: ", id, "\n"))))
   
-  dfs_list_1_names <- names(dfs_list_1)
-  # print(dfs_list_1_names)
+  # Get the files associated with each unique ID
+  id_files <- list.files(path_to_summaries, pattern = id)
+
+  # Test that there are exactly 2 files associated with each unique ID
+  if (length(id_files) == 2L) {
+    # Get each XLSX file as a list of dfs
+    list_dfs1 <- listify_xlsx(id_files[1], path = path_to_summaries)
+    list_dfs2 <- listify_xlsx(id_files[2], path = path_to_summaries)
+    # Compare the list of dfs
+    compare_dfs_list(list_dfs1, list_dfs2)
+  } else if (length(id_files) < 2L) {
+    warning(paste0("There are fewer than 2 files for STUD ID ", id))
+  } else if (length(id_files) > 2L) {
+    warning(paste0("There are more than 2 files for STUD ID ", id))
+  }
+  
 }
-
-compare_dfs_list(dfs_inc1, dfs_inc2)
-
-
-
-
-
-
-
-
-
-
-
 
---
+++ STUD_DDE_Reconcile_helpers.R
@@ -0,0 +1,107 @@
+# STUD_DDE_Reconcile_helpers.R
+
+suppressMessages( library(readxl)  )
+suppressMessages( library(crayon)  )
+
+# Convert XLSX file to a list of worksheet dfs
+listify_xlsx <- function(file, path = NULL) {
+  # Create a df for each pertinent worksheet
+  df_evalpre <-
+    read_xlsx(paste0(path, file),
+              sheet = "Evaluation (Pre)",
+              range = "A1:L39",
+              col_names = LETTERS[1:which(LETTERS == "L")],
+              col_types = "text")
+  df_evalpost <-
+    read_xlsx(paste0(path, file),
+              sheet = "Evaluation (Post)",
+              range = "A1:L39",
+              col_names = LETTERS[1:which(LETTERS == "L")],
+              col_types = "text")
+  df_tbpre <-
+    read_xlsx(paste0(path, file),
+              sheet = "Toolbox (Pre)",
+              range = "A1:K7",
+              col_names = LETTERS[1:which(LETTERS == "K")],
+              col_types = "text")
+  df_tbpost <-
+    read_xlsx(paste0(path, file),
+              sheet = "Toolbox (Post)",
+              range = "A1:K7",
+              col_names = LETTERS[1:which(LETTERS == "K")],
+              col_types = "text")
+  df_sess1 <-
+    read_xlsx(paste0(path, file),
+              sheet = "Session 1",
+              range = "A1:E41",
+              col_names = LETTERS[1:which(LETTERS == "E")],
+              col_types = "text")
+  df_sess2 <-
+    read_xlsx(paste0(path, file),
+              sheet = "Session 2",
+              range = "A1:E41",
+              col_names = LETTERS[1:which(LETTERS == "E")],
+              col_types = "text")
+  df_sess3 <-
+    read_xlsx(paste0(path, file),
+              sheet = "Session 3",
+              range = "A1:E41",
+              col_names = LETTERS[1:which(LETTERS == "E")],
+              col_types = "text")
+  df_sess4 <-
+    read_xlsx(paste0(path, file),
+              sheet = "Session 4",
+              range = "A1:E41",
+              col_names = LETTERS[1:which(LETTERS == "E")],
+              col_types = "text")
+  # Return all dfs as a list
+  list("file"              = file, 
+       "Evalution (Pre)"   = df_evalpre,
+       "Evaluation (Post)" = df_evalpost,
+       "Toolbox (Pre)"     = df_tbpre,
+       "Toolbox (Post)"    = df_tbpost,
+       "Session 1"         = df_sess1,
+       "Session 2"         = df_sess2,
+       "Session 3"         = df_sess3,
+       "Session 4"         = df_sess4)
+}
+
+
+# Custom comparison of lists containing worksheet dfs
+compare_dfs_list <- function(dfs_list_1, dfs_list_2) {
+  dfs_list_1_name <- rlang::as_string(rlang::ensym(dfs_list_1))
+  dfs_list_2_name <- rlang::as_string(rlang::ensym(dfs_list_2))
+  
+  dfs_list_1_names <- names(dfs_list_1)[-1]
+  dfs_list_2_names <- names(dfs_list_2)[-1]
+  
+  # cat(paste0(xlsx1, " / \n", xlsx2, "\n"))
+  cat(paste0(dfs_list_1[["file"]], " / \n", dfs_list_2[["file"]], "\n"))
+  
+  if (identical(dfs_list_1[-1], dfs_list_2[-1])) {
+    cat(bold(green(paste0("  Speadsheets match! \U1F197\n\n"))))
+    # print(paste0("  Speadsheets match! \U1F197\n\n"))
+  } else {
+    for (name in dfs_list_1_names) {
+      if (identical(dfs_list_1[[name]], dfs_list_2[[name]])) {
+        # cat(paste0("  Match \U2714\n",
+        #            "    in ", name, "\n"))
+      } else {
+        cat(bold(red(paste0("  Mismatch\U00A1 "))))
+        cat(paste0("\U274C\n", "    in ", bold(underline(name)), "\n"))
+        for (i in seq_len(nrow(dfs_list_1[[name]]))) {
+          for (j in seq_len(ncol(dfs_list_1[[name]]))) {
+            if (!identical(dfs_list_1[[name]][[i, j]], 
+                           dfs_list_2[[name]][[i, j]])) {
+              cat(paste0("    at Row ", bold(underline(i)), 
+                         " Column ", bold(underline(LETTERS[j])), "\n"))
+            }
+          } # end for (j ...)
+        } # end for (i ...)
+      } # end if-else
+    } # end for (name ...)
+    cat("\n")
+  } # end if-else
+  
+}
+
